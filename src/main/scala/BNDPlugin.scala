60291a7b1c2d7a4fd7b919a677b47b618dd5fcba
Johan Haleby <johan.haleby@gmail.com>
2010-05-05 16:09:10 +0200

    Removed debug info

--- src/main/scala/BNDPlugin.scala
+++ src/main/scala/BNDPlugin.scala
@@ -14,10 +14,6 @@ import java.io.FileOutputStream
 import java.util.jar.Attributes
 import java.util.List
 import java.util.LinkedList
-import sbt.Path
-import sbt.PathFinder
-import sbt.FileUtilities
-import java.util.jar.Manifest
 
 trait BNDPlugin extends DefaultProject with BNDPluginProperties {
   protected val project = this
@@ -31,60 +27,6 @@ trait BNDPlugin extends DefaultProject with BNDPluginProperties {
    manifestInfo
   }
   
-  override protected def packageAction = {
-    def myPackageTask(sources: PathFinder, jarPath: => Path, options: => Seq[PackageOption]): Task =
-		fileTask("package", jarPath from sources)
-		{
-		    log info "In myPackageTask with options %s".format(options.toString)
-			import com.weiglewilczek.bnd4sbt.test.{MutableMapWrapper,Wrappers}
-			/** Copies the mappings in a2 to a1, mutating a1. */
-			def mergeAttributes(a1: Attributes, a2: Attributes)
-			{
-				for( (key, value) <- Wrappers.toList(a2))
-					a1.put(key, value)
-			}
-
-			val manifest = new Manifest
-			var recursive = false
-			for(option <- options)
-			{
-				option match
-				{
-					case JarManifest(mergeManifest) =>
-					{
-						mergeAttributes(manifest.getMainAttributes, mergeManifest.getMainAttributes)
-						val entryMap = new MutableMapWrapper(manifest.getEntries)
-						for((key, value) <- Wrappers.toList(mergeManifest.getEntries))
-						{
-							entryMap.get(key) match
-							{
-								case Some(attributes) => mergeAttributes(attributes, value)
-								case None => entryMap += (key, value)
-							}
-						}
-					}
-					case Recursive => recursive = true
-					case MainClass(mainClassName) =>
-						manifest.getMainAttributes.put(Attributes.Name.MAIN_CLASS, mainClassName)
-					case ManifestAttributes(attributes @ _*) =>
-						val main = manifest.getMainAttributes
-						for( (name, value) <- attributes) {
-							println("Adding %s : %s to manifest".format(name, value))
-							main.put(name, value)
-						}
-					case _ => log.warn("Ignored unknown package option " + option)
-				}
-			}
-			val jarPathLocal = jarPath
-			FileUtilities.clean(jarPathLocal :: Nil, log) orElse
-			FileUtilities.jar(sources.get, jarPathLocal, manifest, recursive, log)
-		}
-    	log info "Before my package task"
-    	val returnVal = myPackageTask(packagePaths, jarPath, packageOptions).dependsOn(compile) describedAs "PackageDescription"
-        log info "After my package task"
-        returnVal
-    }
-
   private def properties  = {
     val properties = Map(
     		"Bundle-SymbolicName"-> bndBundleSymbolicName,
