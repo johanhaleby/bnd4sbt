661c70cbb913f3f423584120f90b0dffb31126fb
Johan Haleby <johan.haleby@gmail.com>
2010-05-06 09:28:43 +0200

    Using manifest mainattributes

--- src/main/scala/BNDPlugin.scala
+++ src/main/scala/BNDPlugin.scala
@@ -14,28 +14,37 @@ import java.io.FileOutputStream
 import java.util.jar.Attributes
 import java.util.List
 import java.util.LinkedList
+import java.util.jar.Manifest
 
 trait BNDPlugin extends DefaultProject with BNDPluginProperties {
   protected val project = this
 
   override def packageOptions: Seq[PackageOption] = {
-    val props = properties; 
-    val osgiManifestAttributes = props.map(entry => ManifestAttributes( (new Attributes.Name(entry._1), entry._2) )).toList
-	val manifestInfo = manifestClassPath.map(cp => ManifestAttributes( (Attributes.Name.CLASS_PATH, cp) )).toList :::
-	        getMainClass(false).map(MainClass(_)).toList ::: osgiManifestAttributes
-   log info "Created manifest info: %s".format(manifestInfo)
-   manifestInfo
+    log info "JOhan!!!"
+    val mainAttributes = createManifest().getMainAttributes()
+    val manifestEntries = (new scala.collection.immutable.HashSet() ++ new scala.collection.jcl.HashSet[java.util.Map.Entry[Attributes.Name,String]](mainAttributes.entrySet().asInstanceOf[java.util.HashSet[java.util.Map.Entry[Attributes.Name,String]]]))
+    val osgiManifestAttributes = manifestEntries.map(entry => ManifestAttributes( (entry.getKey, entry.getValue) )).toList
+	val manifestInfo = getMainClass(false).map(MainClass(_)).toList ::: osgiManifestAttributes //manifestClassPath.map(cp => ManifestAttributes( (Attributes.Name.CLASS_PATH, cp) )).toList :::
+    manifestInfo
   }
   
-  private def properties  = {
-    val properties = Map(
-    		"Bundle-SymbolicName"-> bndBundleSymbolicName,
-    		"Bundle-Version" -> bndBundleVersion,
-    		"Bundle-Name" -> bndBundleName,
-    		"Private-Package" -> (bndPrivatePackage mkString ","),
-    		"Export-Package" -> (bndExportPackage mkString ","),
-    		"Import-Package"-> (bndImportPackage mkString ",")
-    	)
+ private def createManifest() : Manifest = {
+    val builder = new Builder
+    builder setProperties properties
+    builder setClasspath Array(bndClasspath.absolutePath)
+    val jar = builder.build
+    val manifest =jar.getManifest
+    manifest
+  }
+
+  private def properties = {
+    val properties = new Properties
+    properties.setProperty("Bundle-SymbolicName", bndBundleSymbolicName)
+    properties.setProperty("Bundle-Version", bndBundleVersion)
+    properties.setProperty("Bundle-Name", bndBundleName)
+    properties.setProperty("Private-Package", bndPrivatePackage mkString ",")
+    properties.setProperty("Export-Package", bndExportPackage mkString ",")
+    properties.setProperty("Import-Package", bndImportPackage mkString ",")
     properties
   }
 }
